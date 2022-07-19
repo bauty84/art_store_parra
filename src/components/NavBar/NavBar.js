f105ca48f6171b3a1d1fa436215f44fe0ce1c636
Juan Bautista Parra <juanbautistaparra@outlook.com>
2022-07-19 19:10:28 -0300

    Menu agregado, no puedo hacer que tome la imagen del logo por alguna razon

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,8 +3,15 @@ import './NavBar.scss';
 
 const NavigationBar = () => {
     return (
-        <div>
-            <p>MENU</p>
+        <div className='navbar-main'>
+            <img src='/assets/logo.svg' alt='Art Store' />
+            <ul>
+                <li><button>Inicio</button></li>
+                <li><button>Productos</button></li>
+                <li><button>Servicios</button></li>
+                <li><button>Quienes Somos</button></li>
+                <li><button>Contacto</button></li>
+            </ul>
         </div>
     )
 }
